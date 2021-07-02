0b057a4ff489c0fe6dc2ac60ce00dfca1d08d4ba
Zachary Hallemeyer <zhallemeyer@gmail.com>
2021-07-01 19:51:31 -0700

    Added environment spawner based on server and added player movement and player rotation

---
+++ Assets/Scripts/PlayerCamController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamController : MonoBehaviour
+{
+    public Transform orientation;
+    //public float sensitivity = 100f;
+    public float clampAngle = 85f;
+
+    private float verticleRotation;
+    private float horizontalRotation;
+
+    // Camera
+    public Transform playerCamPosition;
+    private float mouseX;
+    private float mouseY;
+    private float xRotation;
+    private float desiredX;
+    private float normalFOV = 60;
+    private float adsFOV = 40;
+    public float sensitivity = 2000f;
+    private float normalSensitivity = 2000f;
+    private float adsSensitivity = 500f;
+    // Default value for sens multipliers are 1 
+
+    public float sensMultiplier { get; set; } = 1f;
+    public float adsSensMultiplier { get; set; } = 1f;
+
+    private void Start()
+    {
+        Cursor.visible = !Cursor.visible;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        verticleRotation = transform.localEulerAngles.x;
+        horizontalRotation = transform.localEulerAngles.y;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ToggleCursorMode();
+        if (Cursor.lockState == CursorLockMode.Locked)
+            Look();
+        Debug.DrawRay(transform.position, transform.forward * 2, Color.green);
+    }
+
+    private void Look()
+    {
+        /*
+        float _mouseVertical = -Input.GetAxis("Mouse Y");
+        float _mouseHorizontal = Input.GetAxis("Mouse X");
+
+        verticleRotation += _mouseVertical * sensitivity * Time.deltaTime;
+        horizontalRotation += _mouseHorizontal * sensitivity * Time.deltaTime;
+
+        verticleRotation = Mathf.Clamp(verticleRotation, -clampAngle, clampAngle);
+
+        transform.localRotation = Quaternion.Euler(verticleRotation, horizontalRotation, 0);
+        orientation.localRotation = Quaternion.Euler(orientation.localRotation.eulerAngles.x, 
+                                                     horizontalRotation,
+                                                     orientation.localRotation.eulerAngles.z);
+        */
+        /*
+        mouseX = Camera.main.ScreenToViewportPoint(inputMaster.Player.MouseLook.ReadValue<Vector2>()).x
+               * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        mouseY = Camera.main.ScreenToViewportPoint(inputMaster.Player.MouseLook.ReadValue<Vector2>()).y
+                       * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        */
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime * sensMultiplier;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * sensMultiplier;
+
+        //Find current look rotation
+        Vector3 rot = transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+
+        //Rotate, and also make sure we dont over- or under-rotate.
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
+        // Perform the rotations
+        transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+        orientation.localRotation = Quaternion.Euler(0, desiredX, 0);
+        // Make the server handle camera rotation (empty game object on server side) so that player can shoot from cam forward on server
+    }
+
+    private void ToggleCursorMode()
+    {
+        Cursor.visible = !Cursor.visible;
+
+        if (Cursor.lockState == CursorLockMode.None)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int id;
+    public string username;
+    public float health;
+    public float maxHealth;
+    public int itemCount = 0;
+    public MeshRenderer model;
+
+    public void Initialize(int _id, string _username)
+    {
+        id = _id;
+        username = _username;
+        health = maxHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        SendInputToServer();
+    }
+
+    public void SetHealth(float _health)
+    {
+        health = _health;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        model.enabled = false;
+    }
+
+    public void Respawn()
+    {
+        model.enabled = true;
+        SetHealth(maxHealth);
+    }
+
+    private void SendInputToServer()
+    {
+        bool[] _inputsBools = new bool[]
+        {
+            Input.GetKey(KeyCode.W),
+            Input.GetKey(KeyCode.S),
+            Input.GetKey(KeyCode.A),
+            Input.GetKey(KeyCode.D),
+            Input.GetKey(KeyCode.Space)
+        };
+
+        Vector2[] _inputVector2 = new Vector2[]
+        {
+            new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"))
+        };
+
+        ClientSend.PlayerMovement(_inputsBools, _inputVector2);
+    }
+}
